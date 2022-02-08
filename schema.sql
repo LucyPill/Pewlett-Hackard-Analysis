c101da59dde3a5e497894d0877ef98bf53f8988c
Lucy Pepe <lucypill4@gmail.com>
2022-02-07 18:12:32 -0800

    Adding queries

--- schema.sql
+++ schema.sql
@@ -1,13 +1,13 @@
 -- Creating tables fo PH-EmployeeDB
 CREATE TABLE departments(
-	dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	dept_name VARCHAR(40) NOT NULL,
 	PRIMARY KEY (dept_no),
 	UNIQUE (dept_name)
 );
 
 CREATE TABLE employees(
-	emp_no INT NOT NULL,
+	emp_no VARCHAR NOT NULL,
 	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE employees(
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
@@ -26,6 +26,7 @@ CREATE TABLE dept_manager (
 	PRIMARY KEY(emp_no, dept_no)
 );
 
+S
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
@@ -36,14 +37,15 @@ CREATE TABLE salaries(
 );
 
 CREATE TABLE dept_emp(
-	dept_no VARCHAR (4) NOT NULL,
-	emp_no INT NOT NULL,
+	emp_no VARCHAR (10) NOT NULL,
+	dept_no VARCHAR (10) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments( dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, dept_no)
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY(emp_no, dept_no)
 );
+SELECT * FROM dept_emp;
 
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
